b8ba8e16238d10e1dfb355359b57bdd1d2dd1edc
Fabian Dammekens <fabian.dammekens@cegeka.be>
2018-01-17 17:20:11 +0100

    [IAC] Fix syntax for fail() function

--- manifests/params.pp
+++ manifests/params.pp
@@ -58,7 +58,7 @@ class keepalived::params {
     }
 
     default: {
-      fail "Operating system ${::operatingsystem} is not supported."
+      fail("Operating system ${::operatingsystem} is not supported.")
     }
   }
 }
